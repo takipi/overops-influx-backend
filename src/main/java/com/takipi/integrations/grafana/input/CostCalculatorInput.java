6db2da05e81e674e08465779ca146f7cddf33e47
Lorenzo Conti <contil@gmail.com>
2019-01-30 17:15:44 -0800

    minor comment cleanup

--- src/main/java/com/takipi/integrations/grafana/input/CostCalculatorInput.java
+++ src/main/java/com/takipi/integrations/grafana/input/CostCalculatorInput.java
@@ -1,8 +1,6 @@
 package com.takipi.integrations.grafana.input;
 
 public class CostCalculatorInput extends EventsInput {
-
-	//public CostData costData = new CostData();
 	
 	public Double tableLimit;
 }
