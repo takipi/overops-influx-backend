1227f9f483a4f4f16db355322dc1945852a8b5cb
Dor Levi <dor.levi@overops.com>
2019-03-14 14:06:24 +0200

    Move get event request to new cache

--- core/src/main/java/com/takipi/integrations/grafana/cache/key/CacheKeyFactory.java
+++ core/src/main/java/com/takipi/integrations/grafana/cache/key/CacheKeyFactory.java
@@ -3,6 +3,7 @@ package com.takipi.integrations.grafana.cache.key;
 import com.takipi.api.client.ApiClient;
 import com.takipi.api.client.request.application.ApplicationsRequest;
 import com.takipi.api.client.request.deployment.DeploymentsRequest;
+import com.takipi.api.client.request.event.EventRequest;
 import com.takipi.api.client.request.process.JvmsRequest;
 import com.takipi.api.client.request.server.ServersRequest;
 import com.takipi.api.client.request.view.ViewsRequest;
@@ -13,6 +14,7 @@ import com.takipi.integrations.grafana.cache.key.clients.CacheGetApplicationsKey
 import com.takipi.integrations.grafana.cache.key.clients.CacheGetDeploymentsKey;
 import com.takipi.integrations.grafana.cache.key.clients.CacheGetJvmsKey;
 import com.takipi.integrations.grafana.cache.key.clients.CacheGetServersKey;
+import com.takipi.integrations.grafana.cache.key.events.CacheGetEventKey;
 import com.takipi.integrations.grafana.cache.key.views.CacheGetViewKey;
 
 public class CacheKeyFactory
@@ -38,6 +40,10 @@ public class CacheKeyFactory
 		else if (request instanceof ViewsRequest)
 		{
 			return CacheGetViewKey.create(apiClient, (ViewsRequest) request);
+		}
+		else if (request instanceof EventRequest)
+		{
+			return CacheGetEventKey.create(apiClient, (EventRequest) request);
 		}
 		
 		return null;
