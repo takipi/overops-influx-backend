26548ee06feb2985d00fcf1ef649b777a287533f
noam.matalon <noam.matalon@takipi.com>
2019-03-27 18:53:58 +0200

    Add missing field

--- core/src/main/java/com/takipi/integrations/grafana/functions/ReliabilityKpiGraphFunction.java
+++ core/src/main/java/com/takipi/integrations/grafana/functions/ReliabilityKpiGraphFunction.java
@@ -295,6 +295,7 @@ public class ReliabilityKpiGraphFunction extends BaseGraphFunction {
 		protected int slowdowns;
 		protected int severeSlowdowns;
 		protected Map<TransactionKey, TransactionData> transactionMap;
+		protected RegressionInput regressionInput;
 		
 		protected SlowdownInterval(Pair<DateTime, DateTime> period) {
 			super(period);
